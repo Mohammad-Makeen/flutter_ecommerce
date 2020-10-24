47d179e580d2c08df251baa5ac166e7cf6730148
Mohammad Makeen <kings89games@users.noreply.github.com>
2020-10-24 07:32:08 +0300

    initiale comment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fa18afd855196883780d393aa7166a18'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '11aa2893-0172-441e-aee6-81fce5088f6b'
+};
